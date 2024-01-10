c4b3075bed3b3a8bde52e591cd2c80ceb775fac9
Parth Vachhani <vachhani.parth.96@gmail.com>
2024-01-10 11:05:50 -0500

    Complete Finch Collector Part 2

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render
 
-finches = [
-    {
-        "name": "European Goldfinch",
-        "description": "A small passerine bird in the finch family that is native to Europe, North Africa and western Asia.",
-        "size": "Small",
-    },
-    {
-        "name": "Eurasian Chaffinch",
-        "description": "A common and widespread small passerine bird in the finch family.",
-        "size": "Small",
-    },
-    {
-        "name": "Purple Finch",
-        "description": "A small finch that breeds in coniferous and mixed forests of Canada and the northeastern United States.",
-        "size": "Small",
-    },
-    {
-        "name": "House Finch",
-        "description": "A bird in the finch family native to western North America.",
-        "size": "Small",
-    },
-    {
-        "name": "Common Rosefinch",
-        "description": "A medium-sized passerine bird in the finch family.",
-        "size": "Medium",
-    },
-]
+from .models import Finch
+
+# finches = [
+#     {
+#         "name": "European Goldfinch",
+#         "description": "A small passerine bird in the finch family that is native to Europe, North Africa and western Asia.",
+#         "size": "Small",
+#     },
+#     {
+#         "name": "Eurasian Chaffinch",
+#         "description": "A common and widespread small passerine bird in the finch family.",
+#         "size": "Small",
+#     },
+#     {
+#         "name": "Purple Finch",
+#         "description": "A small finch that breeds in coniferous and mixed forests of Canada and the northeastern United States.",
+#         "size": "Small",
+#     },
+#     {
+#         "name": "House Finch",
+#         "description": "A bird in the finch family native to western North America.",
+#         "size": "Small",
+#     },
+#     {
+#         "name": "Common Rosefinch",
+#         "description": "A medium-sized passerine bird in the finch family.",
+#         "size": "Medium",
+#     },
+# ]
 
 
 # Create your views here.
@@ -39,6 +41,13 @@ def about(request):
 
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, "finches/index.html", {
         "finches": finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, "finches/detail.html", {
+        "finch": finch
     })
